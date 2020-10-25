c0e7cb20e03a1b708ad62a6630719520c5930cdd
HalynaHera <gerasymenko_ba12@nuwm.edu.ua>
2020-10-25 23:04:58 +0200

    feat: conplate test taskc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const conf = {};
+  bracketsConfig.forEach(item =>{
+      if(!conf[item[0]]){
+        conf[item[0]] = {
+          open: item[0],
+          count : 0
+        };
+        conf[item[1]] = {
+          close: item[1],
+          count : 0
+        };
+      }
+  });
+
+  [...str].forEach(item =>{
+    if(conf[item]){
+      conf[item].count++;
+    }
+});
+
+let res = true;
+
+bracketsConfig.forEach(item =>{
+  if(conf[item[0]]){
+      if(conf[item[0]].count -  conf[item[1]].count !== 0 ) res = false;
+    };
+});
+
+
+  return res;
 }
